ecc5b7c797f2814a5b921ad30a8510510f41413e
root <root@ip-172-31-45-102.ap-northeast-3.compute.internal>
2026-02-01 17:31:55 +0000

    frontend complete

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,13 +1,57 @@
 function register() {
-    alert("Registered successfully (backend later)");
-    window.location.href = "index.html";
+    fetch("/api/register/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: document.getElementById("reg-username").value,
+            password: document.getElementById("reg-password").value
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.message) {
+            alert(data.message);
+            window.location.href = "index.html";
+        } else {
+            alert(data.error);
+        }
+    })
+    .catch(err => {
+        alert("Server error");
+        console.log(err);
+    });
 }
 
 function login() {
-    alert("Login successful (backend later)");
-    window.location.href = "welcome.html";
+    fetch("/api/login/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: document.getElementById("login-username").value,
+            password: document.getElementById("login-password").value
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.message) {
+            // SUCCESS
+            window.location.href = "welcome.html";
+        } else {
+            // FAILURE
+            alert(data.error);
+        }
+    })
+    .catch(err => {
+        alert("Server error");
+        console.log(err);
+    });
 }
 
 function logout() {
     window.location.href = "index.html";
 }
+
