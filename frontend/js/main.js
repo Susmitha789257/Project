61f323d6ae0ac7569ac63ea85a7a5728d49c38d3
root <root@SUSMITHA.localdomain>
2026-02-01 15:12:35 +0000

    frontend init

---
+++ frontend/js/main.js
@@ -0,0 +1,13 @@
+function register() {
+    alert("Registered successfully (backend later)");
+    window.location.href = "index.html";
+}
+
+function login() {
+    alert("Login successful (backend later)");
+    window.location.href = "welcome.html";
+}
+
+function logout() {
+    window.location.href = "index.html";
+}
